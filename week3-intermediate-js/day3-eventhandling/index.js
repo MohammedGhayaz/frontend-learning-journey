2d9399917728b8871fd27153944ef2ff6138ce6e
MohammedGhayaz <mohammedeyaad@gmail.com>
2025-04-21 17:24:09 +0530

    Day 3: Event handling

--- week3-intermediate-js/day3-eventhandling/index.js
+++ week3-intermediate-js/day3-eventhandling/index.js
@@ -1,6 +1,17 @@
 const btn = document.querySelector('.btn');
+const h1 = document.querySelector('h1');
+const resetButton = document.querySelector('.reset');
 let count = 0;
 btn.addEventListener('click',()=>{
     count++;
-    alert(`You have clicked the button ${count} times`);
+    h1.innerHTML = `You have clicked the button ${count} times`;
+})
+btn.addEventListener('dblclick',()=>{
+    count--;
+    h1.innerHTML = `You have clicked the button ${count} times`;
+})
+
+resetButton.addEventListener('click',()=>{
+    count = 0;
+    h1.innerHTML = `You have clicked the button ${count} times`;
 })
