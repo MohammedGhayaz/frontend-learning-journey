89adbbe2d6178ab8324edc6e7cc5d8a3f7126e0f
MohammedGhayaz <mohammedeyaad@gmail.com>
2025-05-01 20:44:49 +0530

    Day 6: Async/Await error handling

---
+++ week4-async-js/day6-async-errorhandling/script.js
@@ -0,0 +1,44 @@
+const loginUser = () => {
+    return new Promise((resolve, reject) => {
+        const username = 'admin';
+        const password = 1234;
+
+        if(username == 'admin' && password == 1234){
+            console.log('User Logged in');
+            resolve('Success');
+        }
+        else{
+            console.log('Invalid Credentials');
+            reject('Failed');
+        }
+    })
+};
+
+const fethUserProfile = () => {
+    return new Promise((resolve, reject)=>{
+        
+        const randomNumber = Math.round( Math.random() * 100 );
+        setTimeout(()=>{
+            if(randomNumber >= 40){
+                console.log('User profile fetched');
+                resolve('Success');
+            }
+            else{
+                console.log('Unable to fetch user profile');
+                reject('Failed');
+            }
+        },2000);
+
+    })
+}
+
+async function loginProcess(){
+    try{
+        await loginUser();
+        console.log('Fetching User Profile');
+        await fethUserProfile();
+    }
+    catch(err){
+        console.log(err);
+    }
+}
