2ecc291d5a3809e4a627cd626c59393eefe1ec3f
MohammedGhayaz <mohammedeyaad@gmail.com>
2025-05-02 22:00:56 +0530

    DAy 7: Async Task Done

--- week4-async-js/day7-async-task/script.js
+++ week4-async-js/day7-async-task/script.js
@@ -1,2 +1,57 @@
-const orderId = Math.random() * 3000;
+const placeOrder = () => {
+    return new Promise((resolve, reject) => {
+        const orderIdd = Math.round(Math.random() * 3000);
+        setTimeout(()=>{
+            console.log('Order Placed');
+            resolve(orderIdd);
+        },1000);
+    })
+}
 
+const prepareOrder =(orderId) => {
+    return new Promise ((resolve, reject)=>{
+        setTimeout(()=>{
+            console.log(`Order prepared for ${orderId}`);
+            resolve('Prepared');
+        },2000)
+    })
+}
+
+const deliverOrder = (status) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            if(status == 'Prepared'){
+                resolve('Order Delivered');
+            }
+            else{
+                reject('Order Failed');
+            }
+        },1500)
+    })
+}
+
+
+
+// placeOrder().then((res)=>{
+//     return prepareOrder(res);
+// })
+// .then((res)=>{
+//     return deliverOrder(res);
+// })
+// .then((res)=>{
+//     console.log(res);
+// })
+// .catch((err)=>{
+//     console.log(err);
+// })
+
+(async function (){
+    try{
+        const orderId = await placeOrder();
+        const status = await prepareOrder(orderId);
+        console.log(await deliverOrder(status));
+    }
+    catch(err){
+        console.log(err);
+    }
+})();
