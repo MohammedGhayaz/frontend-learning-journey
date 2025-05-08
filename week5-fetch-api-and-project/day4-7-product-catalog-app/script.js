49bf9cb0cd094d899a78e5c45de5b6417e907c6c
MohammedGhayaz <mohammedeyaad@gmail.com>
2025-05-08 20:22:27 +0530

    Day 6: Added try catch block to deal with errors and prevent failure

--- week5-fetch-api-and-project/day4-7-product-catalog-app/script.js
+++ week5-fetch-api-and-project/day4-7-product-catalog-app/script.js
@@ -3,30 +3,34 @@ const inputElement = document.querySelector('.input-div input');
 let allProducts = [];
 let html = '';
 (async function getProducts(){
-  const response = await fetch('https://fakestoreapi.com/products');
-  const data = await response.json();
-  allProducts = data;
-  data.forEach(e => {
-    html += `<div class="grid-items">
+  try{
+    const response = await fetch('https://fakestoreapi.com/products');
+    const data = await response.json();
+    allProducts = data;
+    data.forEach(e => {
+      html += `<div class="grid-items">
     <div class="image">
       <img src="${e.image}" alt="Image">
     </div>
     <div class="title">
-      ${e.title}
+    ${e.title}
     </div>
     <div class="description">
-     ${e.description}
+    ${e.description}
     </div>
     <div class="price">
-      $${e.price}
+    $${e.price}
     </div>
     <div class="button">
-      <button> Buy Now </button>
+    <button> Buy Now </button>
     </div>
     </div>`;
   });
   productsContainer.innerHTML = html;
-})();
+}
+catch(err){
+alert(err);
+}})();
 
 inputElement.addEventListener('input',async ()=>{
   const searchInput = inputElement.value;
