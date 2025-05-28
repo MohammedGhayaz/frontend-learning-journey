8db200a03c559eed51b19f2fe6b454a89a214985
MohammedGhayaz <mohammedeyaad@gmail.com>
2025-05-28 21:02:22 +0530

    Lesson 10 - Excercise Solved

---
+++ week8-practice/html-css-js/10a/script.js
@@ -0,0 +1,25 @@
+// Excercise 10c
+console.log(document.querySelector('.js-buttonn').classList.contains('js-buttonn'));
+
+// Excercise 10d,10e,10f
+
+const gamingButton = document.querySelectorAll('.js-gaming-button');
+
+gamingButton.forEach((button)=>{
+  button.addEventListener('click', ()=>{
+    if(button.classList.contains('is-toggled')){
+      gamingButton.forEach((button)=>{
+        button.classList.remove('is-toggled');
+      })
+    }
+    else{
+      gamingButton.forEach((button)=>{
+        button.classList.remove('is-toggled');
+      })
+      button.classList.add('is-toggled');
+    }
+  });
+})
+
+
+
