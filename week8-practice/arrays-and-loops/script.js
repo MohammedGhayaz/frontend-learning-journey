35f8bc52bf8930cdf373f662184d1a24e0b1823e
MohammedGhayaz <mohammedeyaad@gmail.com>
2025-05-29 21:47:01 +0530

    Arrays and Loops: Solved Excercise 11g to 11h

--- week8-practice/arrays-and-loops/script.js
+++ week8-practice/arrays-and-loops/script.js
@@ -48,3 +48,57 @@ console.log('Excercise 11e');
 for(const element of [0,1,2]){
   console.log(element + 1);
 }
+
+//Excercise 11f
+console.log('Excercise 11f');
+
+function addArray(arr){
+ const newArray =[];
+ arr.forEach((e, index)=> {
+  newArray[index] = e+1;
+  });
+  return newArray;
+}
+//Excercise 11g
+console.log('Excercise 11g');
+
+function addArray(arr, num){
+ const newArray =[];
+ arr.forEach((e, index)=> {
+  newArray[index] = e+num;
+  });
+  return newArray;
+}
+console.log(addArray([10,22], 5));
+
+
+//Excercise 11h
+
+function addArrays(array1,array2){
+  let addedArray = [];
+  console.log(`${array1.length} - ${array2.length}`);
+  if(array1.length === array2.length){
+    for(let i=0;i<array1.length;i++){
+      addedArray[i] = array1[i] + array2[i];
+    }  
+  }
+  else if(array1.length < array2.length){
+    for(let i=0;i<array1.length;i++){
+      addedArray[i] = array1[i] + array2[i];
+    }  
+    for(let i=array1.length; i<array2.length; i++){
+      addedArray[i] = array2[i];
+    }
+  }
+    else{
+      for(let i=0;i<array2.length;i++){
+        addedArray[i] = array1[i] + array2[i];
+      }  
+      for(let i=array2.length; i<array1.length; i++){
+        addedArray[i] = array1[i];
+      }
+    }
+return addedArray;
+}
+
+
