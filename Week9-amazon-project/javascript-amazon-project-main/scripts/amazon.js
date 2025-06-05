0809201c5d3b4c8deb0bb7fc2863215491f140ab
MohammedGhayaz <mohammedeyaad@gmail.com>
2025-06-05 21:50:44 +0530

    Displaying cart total quantity in the cart

--- Week9-amazon-project/javascript-amazon-project-main/scripts/amazon.js
+++ Week9-amazon-project/javascript-amazon-project-main/scripts/amazon.js
@@ -73,6 +73,16 @@ document.querySelectorAll('.js-add-to-cart')
         quantity:1,
       });
     }
+    updateCartQuantity();
     console.log(cart);
   })
- })
+ });
+
+ 
+ function updateCartQuantity(){
+  let cartQuantity = 0;
+  cart.forEach((item)=>{
+    cartQuantity += item.quantity;
+  });
+  document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+}
