d891b829f199a546b90fbfd239a5e40690a6a762
MohammedGhayaz <mohammedeyaad@gmail.com>
2025-07-31 21:31:32 +0530

    create automated tests for removeFromCart.js

--- Week9-amazon-project/javascript-amazon-project-main/tests-jasmine/data/cartTest.js
+++ Week9-amazon-project/javascript-amazon-project-main/tests-jasmine/data/cartTest.js
@@ -1,4 +1,4 @@
-import { addToCart, cart, loadFromStorage } from "../../data/cart.js";
+import { addToCart, cart, loadFromStorage, removeFromCart, saveToStorage } from "../../data/cart.js";
 
 describe('test suite: addToCart', ()=>{
   const productId = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
@@ -67,4 +67,63 @@ describe('test suite: addToCart', ()=>{
   afterEach(()=>{
      document.body.removeChild(inputElement);
    })
+})
+
+
+describe('test suite: removeFromCart()',()=>{
+
+  const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
+  const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
+
+  beforeEach(()=>{
+
+    spyOn(localStorage,'setItem');
+
+    spyOn(localStorage,'getItem').and.callFake(()=>{
+      return JSON.stringify([{
+        productId: productId1,
+        quantity: 1,
+        deliveryOptionId: '1'
+      },{
+        productId: productId2,
+        quantity: 2,
+        deliveryOptionId: '2'
+      }
+      ]);
+    })
+
+    loadFromStorage();
+  })
+
+  it('removes product from the cart', ()=>{
+    
+    removeFromCart(productId1);
+    saveToStorage();
+    expect(cart.length).toEqual(1);
+    expect(cart[0].productId).toEqual(productId2);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart',JSON.stringify([{
+      productId: productId2,
+        quantity: 2,
+        deliveryOptionId: '2'
+    }]))
+  })
+
+  it('works with removing unexisiting product from the cart', ()=>{
+
+    removeFromCart('exampleId');
+    saveToStorage();
+    expect(cart.length).toEqual(2);
+    expect(cart[0].productId).toEqual(productId1);
+    expect(cart[1].productId).toEqual(productId2);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart',JSON.stringify([{
+      productId: productId1,
+      quantity: 1,
+      deliveryOptionId: '1'
+    },
+    {
+      productId: productId2,
+      quantity: 2,
+      deliveryOptionId: '2'
+    }]))
+  })
 })
