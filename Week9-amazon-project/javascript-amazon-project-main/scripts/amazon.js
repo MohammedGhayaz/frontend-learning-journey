83afbcf2249973fd5d3e5c4a2a0b4b06c4eebee2
MohammedGhayaz <mohammedeyaad@gmail.com>
2025-08-04 21:34:00 +0530

    use polymorphism and inheritence to add sizeChart

--- Week9-amazon-project/javascript-amazon-project-main/scripts/amazon.js
+++ Week9-amazon-project/javascript-amazon-project-main/scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach( product => {
   </div>
 
   <div class="product-price">
-    $${product.getPrice()}
+    ${product.getPrice()}
   </div>
 
   <div class="product-quantity-container">
@@ -44,6 +44,8 @@ products.forEach( product => {
   </div>
 
   <div class="product-spacer"></div>
+
+  ${product.extraInfoHTML()}
 
   <div class="added-to-cart js-added-to-cart-${product.id}">
     <img src="images/icons/checkmark.png">
