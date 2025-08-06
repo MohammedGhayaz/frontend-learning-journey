4c7aab21bdaea07f9ea3610784bbb36493abbc4a
Mohammed Ghayaz <=>
2025-08-06 17:08:45 +0530

    create automated tests for clothing class

--- Week9-amazon-project/javascript-amazon-project-main/tests-jasmine/data/productsTest.js
+++ Week9-amazon-project/javascript-amazon-project-main/tests-jasmine/data/productsTest.js
@@ -1,4 +1,4 @@
-import {Product} from '../../data/products.js'
+import {Product, Clothing} from '../../data/products.js'
 import { formatCurrency } from '../../scripts/utils/money.js';
 
 describe('test suite: product class',()=>{
@@ -21,8 +21,7 @@ describe('test suite: product class',()=>{
         })
     })
     
-    it('generate fields of product class', ()=>{
-       
+    it('generate fields of product class', ()=>{       
         expect('id' in product1).toBe(true);
         expect('image' in product1).toBe(true);
         expect('name' in product1).toBe(true);
@@ -32,7 +31,6 @@ describe('test suite: product class',()=>{
     })
 
     it('should initialize Product class with correct field values', ()=>{
-       
         expect(product1.id).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
         expect(product1.image).toEqual('images/products/athletic-cotton-socks-6-pairs.jpg');
         expect(product1.name).toEqual('Black and Gray Athletic Cotton Socks - 6 Pairs');
@@ -41,21 +39,76 @@ describe('test suite: product class',()=>{
                 count: 87
             });
         expect(product1.priceCents).toEqual(1090);
-
     })
 
     it('generate methods of product class', ()=>{
-       
         expect(typeof product1.getStars).toBe('function');
         expect(typeof product1.getPrice).toBe('function');
         expect(typeof product1.extraInfoHTML).toBe('function');
     })
 
     it('should perform expected actions when Product methods are called',()=>{
-
         expect(product1.getStars()).toEqual(`images/ratings/rating-${product1.rating.stars * 10}.png`)
         expect(product1.getPrice()).toEqual(`$${formatCurrency(product1.priceCents)}`)
         expect(product1.extraInfoHTML()).toEqual('');
     })
 
+});
+
+
+describe('test suite: clothing class', ()=>{
+    let clothingProduct;
+    beforeEach(()=>{
+        clothingProduct = new Clothing({
+            id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+            image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+            name: "Adults Plain Cotton T-Shirt - 2 Pack",
+                rating: {
+                    stars: 4.5,
+                    count: 56
+                },
+            priceCents: 799,
+            keywords: [
+                "tshirts",
+                "apparel",
+                "mens"
+            ],
+            type: "clothing",
+            sizeChartLink: "images/clothing-size-chart.png"
+        })
+    })
+
+    it('generate fields of clothing class', ()=>{
+        expect('id' in clothingProduct).toBe(true);
+        expect('image' in clothingProduct).toBe(true);
+        expect('name' in clothingProduct).toBe(true);
+        expect('rating' in clothingProduct).toBe(true);
+        expect('priceCents' in clothingProduct).toBe(true);
+        expect('sizeChartLink' in clothingProduct).toBe(true);
+    })
+
+    it('should initialize Clothing class with correct field values', ()=>{
+       
+        expect(clothingProduct.id).toEqual('83d4ca15-0f35-48f5-b7a3-1ea210004f2e');
+        expect(clothingProduct.image).toEqual('images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg');
+        expect(clothingProduct.name).toEqual('Adults Plain Cotton T-Shirt - 2 Pack');
+        expect(clothingProduct.rating).toEqual({
+                stars: 4.5,
+                count: 56
+            });
+        expect(clothingProduct.priceCents).toEqual(799);
+        expect(clothingProduct.sizeChartLink).toEqual('images/clothing-size-chart.png');
+    })
+    
+    it('generate methods of clothing class', ()=>{       
+        expect(typeof clothingProduct.getStars).toBe('function');
+        expect(typeof clothingProduct.getPrice).toBe('function');
+        expect(typeof clothingProduct.extraInfoHTML).toBe('function');
+    })
+
+    it('should perform expected actions when Clothing methods are called',()=>{
+        expect(clothingProduct.getStars()).toEqual(`images/ratings/rating-${clothingProduct.rating.stars * 10}.png`)
+        expect(clothingProduct.getPrice()).toEqual(`$${formatCurrency(clothingProduct.priceCents)}`)
+        expect(clothingProduct.extraInfoHTML()).toContain('Size Chart');
+    })
 })
