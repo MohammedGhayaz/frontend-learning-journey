7a9a8121ab5ae9ebb0e6ba7274c0f3b42c542340
MohammedGhayaz <mohammedeyaad@gmail.com>
2025-08-07 20:24:31 +0530

    create backend-practice.js file and fetch data from backend

--- Week9-amazon-project/javascript-amazon-project-main/scripts/checkout.js
+++ Week9-amazon-project/javascript-amazon-project-main/scripts/checkout.js
@@ -2,6 +2,7 @@ import { renderCartSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 // import '../data/cart-class.js';
 // import { Car } from "../data/car.js";
+import '../data/backend-practice.js'
 
 renderCartSummary();
 renderPaymentSummary();
