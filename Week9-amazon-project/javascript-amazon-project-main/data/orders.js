3ab57030151a938af566ba61d8371d2be4211355
MohammedGhayaz <mohammedeyaad@gmail.com>
2025-08-08 21:48:21 +0530

    add example URL parameters

--- Week9-amazon-project/javascript-amazon-project-main/data/orders.js
+++ Week9-amazon-project/javascript-amazon-project-main/data/orders.js
@@ -1,5 +1,7 @@
 export const orders = JSON.parse(localStorage.getItem('orders')) || [];
 
+
+
 export function addOrder(order){
   orders.unshift(order);
   saveToStorage();
--- Week9-amazon-project/javascript-amazon-project-main/scripts/checkout/paymentSummary.js
+++ Week9-amazon-project/javascript-amazon-project-main/scripts/checkout/paymentSummary.js
@@ -85,6 +85,8 @@ export function renderPaymentSummary(){
       }
 
       window.location.href = 'orders.html';
+
+
     }
   )
 }
