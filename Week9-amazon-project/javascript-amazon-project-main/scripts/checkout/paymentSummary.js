18559e43902823a2df9968c4506370f7ad4d2c85
MohammedGhayaz <mohammedeyaad@gmail.com>
2025-08-08 21:28:17 +0530

    create order using the backend

--- Week9-amazon-project/javascript-amazon-project-main/scripts/checkout/paymentSummary.js
+++ Week9-amazon-project/javascript-amazon-project-main/scripts/checkout/paymentSummary.js
@@ -3,7 +3,7 @@ import { renderCartQuantity } from "../utils/renderCartQuantity.js"
 import { getProducts, products } from "../../data/products.js"
 import {formatCurrency} from '../utils/money.js'
 import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js"
-
+import { addOrder } from "../../data/orders.js"
 
 
 export function renderPaymentSummary(){
@@ -59,10 +59,32 @@ export function renderPaymentSummary(){
           <div class="payment-summary-money js-total-price">$${formatCurrency(totalPriceCentsAfterTax)}</div>
         </div>
 
-        <button class="place-order-button button-primary">
+        <button class="place-order-button js-place-order button-primary">
           Place your order
         </button>`;
 
   document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHTML;
 
+  document.querySelector('.js-place-order')
+    .addEventListener('click',async ()=>{
+      try{
+
+        const response = await fetch('https://supersimplebackend.dev/orders',{
+          method: 'POST',
+          headers: {
+            'Content-Type':'application/json'
+          },
+          body: JSON.stringify({
+            cart : cart
+          })
+        });
+        const order = await response.json();
+        addOrder(order);
+      } catch(error){
+        console.log('Unexpected error. Try again later');
+      }
+
+      window.location.href = 'orders.html';
+    }
+  )
 }
